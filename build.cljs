0ba35123f14ed482d91ba420322fb3f01e26f7f8
Pankaj Doharey <pankajdoharey@gmail.com>
2018-12-20 19:25:20 +0530

    * Adds WTFPL License, and npm scripts. * Updates the Muon download code as well as install binary.

--- build.cljs
+++ build.cljs
@@ -1,9 +1,47 @@
 (require '[lumo.build.api :as b])
 
-(b/build
- (b/inputs "src")
- {:output-to "main.js"
-  :output-dir "out/"
-  :optimizations :simple
-  :target :nodejs
-  :infer-externs true})
+
+(defn build []
+  (b/build
+   (b/inputs "src")
+   {:main 'muon.muon
+    :output-to "main.js"
+    :output-dir "out/"
+    :optimizations :none
+    :target :nodejs
+    :infer-externs true}))
+
+(defn release []
+  (b/build
+   (b/inputs "src")
+   {:main 'muon.muon
+    :output-to "main.js"
+    :output-dir "out/"
+    :optimizations :simple
+    :target :nodejs
+    :infer-externs true}))
+
+(defn build-bin []
+ (b/build
+  (b/inputs "src")
+  {:main 'muon.install
+   :output-to "bin/muon.js"
+   :output-dir "out/"
+   :optimizations :simple
+   :target :nodejs
+   :infer-externs true})
+  (.log js/console "Muon bin/executable compiled."))
+
+(defn -main []
+  (let [args js/process.argv
+        command (last args)]
+    (.log js/console "Exec script: " command)
+    (case command
+      "build-bin" (build-bin)
+      "release" ((release) (build-bin))
+      (build))))
+
+(-main)
+
+
+
---
+++ src/muon/install.cljs
@@ -0,0 +1,24 @@
+(ns muon.install
+  (:require ["os" :as os]
+            ["path" :as path]
+            ["child_process" :as proc]))
+
+
+(def pkg-resolve (js/require.resolve "mkdirp"))
+;; (def pkg-resolve (js/require.resolve "muon-download"))
+
+(defn bin-name [pkg]
+  (let [os (.platform os)]
+    (case os
+      "darwin" (.join path pkg "/vendor/Brave.app/Contents/MacOS/Brave")
+      "win32" (.join path pkg "/vendor/brave.exe")
+      (.join path pkg "/vendor/brave"))))
+
+(defn main []
+  (let [argv js/process.argv
+        pkg (.dirname path pkg-resolve)
+        bin (bin-name pkg)]
+    (.spawn proc bin (.slice argv 3))
+    ))
+
+(main)
